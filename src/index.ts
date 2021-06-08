4abf5c26cce1604906fedbadcca8e77714d1c8fe
Jonathan Jayet <jonathan@hublo.com>
2021-06-08 17:51:45 +0200

    feat(all project): added a possible solution

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import * as http from 'http'
+import { join } from 'path'
 import { Server } from 'socket.io'
 
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
-app.get('/', (_, res) => {
-  res.send('<h1>Hello world</h1>')
+app.get('/', (_: Request, res: Response) => {
+  res.sendFile(join(__dirname, '..', 'public', 'index.html'))
 })
 
-io.on('connection', (_) => {
+io.on('connection', (socket) => {
   console.log('A new user has connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  socket.on('message', (msg: string) => {
+    io.emit('message', msg)
+  })
 })
 
 server.listen(3000, () => {
